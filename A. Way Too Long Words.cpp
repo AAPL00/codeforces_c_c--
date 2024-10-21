14fb8af42cd7fb7b0577d09f80febbaa6fbca88e
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-21 14:18:24 -0400

    Update A. Way Too Long Words problem solution

--- A. Way Too Long Words.cpp
+++ A. Way Too Long Words.cpp
@@ -5,16 +5,23 @@
 using namespace std;
 int main() {
     int number_of_words;
+    string word;
     scanf("%d", &number_of_words);
     vector<string> list_of_words;
     for (int i = 0; i < number_of_words; i++) {
-        cin >> list_of_words[i];
+        cin >> word;
+        list_of_words.push_back(word);
     }
     char first;
     char last;
     int number;
     for ( int i = 0; i < number_of_words; i++) {
-        cout << list_of_words[i][0] << list_of_words[i].size() - 2 << list_of_words[i][list_of_words[i].size() - 1];
+        if (list_of_words[i].size() > 10) {
+            cout << list_of_words[i][0] << list_of_words[i].size() - 2 << list_of_words[i].back() << endl;
+        }
+        else {
+            cout << list_of_words[i] << endl;
+        }
     }
     return 0;
 }
