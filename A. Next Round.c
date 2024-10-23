9fe45f1cac215ed5aac0610f9b802ba20a3c6945
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-22 20:26:09 -0400

    Update A. Next Round problem solution

--- A. Next Round.c
+++ A. Next Round.c
@@ -1,4 +1,3 @@
-//No acepta la respuesta, da error en un caso de prueba mas escrito con la entrada 5 5
 #include <stdio.h>
 int main() {
     int total_participants;
@@ -7,19 +6,22 @@ int main() {
     scanf("%d %d", &total_participants, &k_place);
     int scores[total_participants];
     for(int i = 0; i < total_participants; i++) {
-        scanf("%d", &scores[i]); //puedes introducirlos en una sola linea y presionar enter al final
+        scanf("%d", &scores[i]);
     }
-    int k_place_score = scores[k_place];
-    if (k_place_score == 0) {
-        printf("%d", number_of_advancers);
-    }
-    else {
-        for(int j = 0; j < total_participants; j++) {
-            if(scores[j] >= k_place_score) {
-            number_of_advancers ++;
+    int k_place_score = scores[k_place - 1];
+    if (k_place_score != 0) {
+        for (int i = 0; i < total_participants; i++) {
+            if (scores[i] >= k_place_score) {
+                number_of_advancers++;
             }
         }
-         printf("%d", number_of_advancers);
     }
-    return 0;  
+    else if (k_place_score == 0) {
+        for (int i = 0; i < total_participants; i++) {
+            if (scores[i] > 0) {
+                number_of_advancers++;
+            }
+        }    
+    }
+    printf("%d", number_of_advancers);
 }
