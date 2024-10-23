b22c18df2f707bb2ba805ac207977e64b983247d
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-23 12:10:51 -0400

    Update A. Helpful Maths problem solution (incomplete)

--- A. Helpful Maths.c
+++ A. Helpful Maths.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
     int one = 0;
     int two = 0;
     int three = 0;
-    char string[100] = {'0'};
+    char string[100];
     int string_size = 0;
-    scanf("%s", &string);
+    scanf("%s", string);
     for(int i = 0; i < 100; i++) {
         switch (string[i]) {
         case '1' : one++; break;
@@ -14,29 +14,23 @@ int main() {
         case '3' : three++;
         }
     }
-    for(int i = 0; i < 100; i++) {
-        if(string[i] != '0') {
-            string_size++;
-        }
-        else {
-            break;
-        }
-    }
-    char newstring[string_size];
+    char newstring[strlen(string)];
+    printf("%d\n %d\n %d\n", one, two, three);
     int i = 0;
-    for(i; i < one; i + 2) {
+    for (i; i <= one; i+=2) {
         newstring[i] = '1';
+        printf("%c", newstring[i]);
     }
-    for(i; i < i + two; i + 2) {
+    for (i; i <= i + two; i+=2) {
         newstring[i] = '2';
     }
-    for(i; i < i + three; i + 2) {
+    for (i; i <= i + two + three; i+=2) {
         newstring[i] = '3';
     }
-    for(int i = 1; i < 99; i + 2) {
+    for (int i = 1; i < strlen(newstring) - 1; i+=2) {
         newstring[i] = '+';
     }
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < strlen(newstring); i++) {
         printf("%c", newstring[i]);
     }
 }
