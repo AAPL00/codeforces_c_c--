9bf1dcd679aa9138a11e943c2dbf1c011e4037e3
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-27 11:44:37 -0400

    Minimal Update

--- A. Word Capitalization.cpp
+++ A. Word Capitalization.cpp
@@ -2,8 +2,8 @@
 int main() {
     char string[1001];
     scanf("%s", string);
-    if (string[0] >= 'a' && string[0] <= 'z') {   //en realidad lo que se compara son los ASCII y la diferencia exacta entre
-        string[0] -= 32;                          //un carácter en minúsculas y su representación en mayusculas es de 32
+    if (string[0] >= 'a' && string[0] <= 'z') {  
+        string[0] -= 32;                          
     }
     printf("%s", string);
     return 0;
